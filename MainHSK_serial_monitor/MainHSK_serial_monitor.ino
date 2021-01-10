7ba84cc7b2845ca666a6ce4920bbc5113488bb11
keithwm422 <keithwm.422@gmail.com>
2021-01-10 15:34:29 -0500

    Correct Overtemp pin on DCT and added Main HSK flowmonitor code.

---
+++ MainHSK_serial_monitor/MainHSK_serial_monitor.ino
@@ -0,0 +1,156 @@
+
+#include <Core_protocol.h>
+#include <PacketSerial.h>
+#include <driverlib/sysctl.h>
+#include <Hsk_all_data_types.h>
+#define DOWNBAUD 115200 // Baudrate for serial monitor
+
+#include "src/MainHsk_IU_tests_lib/Flowmeters.h"
+/* structs for storing data from this devices sensors */
+Flowmeters flow_1;
+double gasdata[6]={0};
+char gastype[100];
+char errorcode[100];
+int baud_flows =19200;
+const char * flow_ID_1= "A";
+sMainFlows mainflows[3];
+sMainFlowsGasString mainflowsgasstring;
+char gas1[100];
+typedef enum {
+  Flowmeters_Idle=0,
+  Flowmeters_Request=1,
+  Flowmeters_Available=2,
+  Flowmeters_Read=3,
+  Flowmeters_Read_Complete=4,
+} Flowmeters_State_t;
+Flowmeters_State_t Flowmeters_State=Flowmeters_Idle;
+int avail_wait_tries=0;
+bool parse_worked;
+#define FLOWMETERS_UPDATE_PERIOD 10000  // period between reads/writes
+unsigned long FlowmetersUpdateTime=0;  // for transmit recording time
+char flowmetersBuffer;
+char flowmetersReadArray[100]={0};
+char id;
+float set2{0};
+char  gas[32] = "";
+char  stat1[32] = "";
+char  stat2[32] = "";
+int flowmeters_read_index=0;
+const char err_msg[20]="NO READ AVAIL.";   
+//double mainflows_a[6]={0}; // to store flowmeter 1 values in mainflows structs
+bool is_high=true;
+int LED=38;
+unsigned long LED_time=0;
+#define LED_PERIOD 1000
+void setup() {
+  Serial.begin(DOWNBAUD);
+
+  //Flowmeters initializations, flow_1 object instance (should be one instance per flowmeter)
+  // Serial connections: 4,6, and 7. 
+  flow_1.begin(Serial7,baud_flows, flow_ID_1);
+  FlowmetersUpdateTime = millis() + FLOWMETERS_UPDATE_PERIOD;
+  Serial.println("Starting...");
+  pinMode(LED,OUTPUT);
+  digitalWrite(LED,HIGH);
+  LED_time=millis();
+  while(flow_1.available()){ // this will just clear the buffer really.
+    flowmetersBuffer=flow_1.read_byte();
+    Serial.print(flowmetersBuffer);
+  }
+  delay(1000);
+}
+
+void loop() {
+  //  i2c one wire bridge conn
+  if ((long) (millis() - LED_time) > 0) {
+    LED_time = millis() + LED_PERIOD;
+    switch_LED();
+
+  }  
+  // read in the flows
+  switch(Flowmeters_State){
+    case Flowmeters_Idle:{
+      if(flow_1.available()){ // this will just clear the buffer really.
+        flowmetersBuffer=(char)flow_1.read_byte();
+        break;
+      }
+      else {  // nothing to read so check to see if time to request a read
+        if ((long) (millis() - FlowmetersUpdateTime) > 0) {
+          FlowmetersUpdateTime = millis() + FLOWMETERS_UPDATE_PERIOD;
+          Flowmeters_State=(Flowmeters_State_t) ((unsigned char) Flowmeters_State + 1);
+          break;
+        }
+        else break;
+      }
+    }
+    case Flowmeters_Request:{
+      flow_1.poll();
+      Flowmeters_State=(Flowmeters_State_t) ((unsigned char) Flowmeters_State + 1);
+      flowmeters_read_index=0;
+      avail_wait_tries=0;
+      parse_worked=false;
+      break;
+    }
+    case Flowmeters_Available:{
+      // setup the chars and the arrays for this flowmeter (probably need to clear something)
+      flow_1.read_setup();
+      Flowmeters_State=(Flowmeters_State_t) ((unsigned char) Flowmeters_State + 1);
+      avail_wait_tries=0;
+      break;
+    }
+    case Flowmeters_Read:{
+      int read_long_val=flow_1.read_byte();
+      if(read_long_val!=-1){
+        flowmetersReadArray[flowmeters_read_index]=(char) read_long_val;
+        flowmeters_read_index++;
+        avail_wait_tries=0; //reset the wait times before checking again?
+        break;
+      }
+      else{
+        avail_wait_tries++;
+        if(avail_wait_tries>=1000) Flowmeters_State=(Flowmeters_State_t) ((unsigned char) Flowmeters_State + 1);
+        break;
+      }
+    }
+    case Flowmeters_Read_Complete:{
+      parse_worked=flow_1.getGasData(gasdata,gastype);
+      if(parse_worked){
+        mainflows[0].pressure=gasdata[0];
+        mainflows[0].temperature=gasdata[1];
+        mainflows[0].volume=gasdata[2];
+        mainflows[0].mass=gasdata[3];
+        mainflows[0].setpoint=gasdata[4];
+        set2=gasdata[5];
+        memcpy((uint8_t*) gas,(uint8_t *) gastype,sizeof(gas));
+        print_packet();
+      }
+      Flowmeters_State=Flowmeters_Idle;
+      break;
+    }
+  }  
+}
+
+void print_packet(){
+  Serial.println("Flowmeter data: ");
+  Serial.print("Pressure ");
+  Serial.print(mainflows[0].pressure,4);
+  Serial.print(", Temperature ");
+  Serial.print(mainflows[0].temperature,4);
+  Serial.print(", Vol Flow ");
+  Serial.print(mainflows[0].volume,4);
+  Serial.print(", Mass Flow ");
+  Serial.print(mainflows[0].mass,4);
+  Serial.print(", Setpoint ");
+  Serial.print(mainflows[0].setpoint,4);
+  Serial.println("  done  ");
+}
+void switch_LED(){
+  if(is_high){
+    is_high=false;
+    digitalWrite(LED,LOW);
+  }
+  else{    
+    is_high=true;
+    digitalWrite(LED,HIGH);
+  }
+}
---
+++ MainHSK_serial_monitor/src/MainHsk_IU_tests_lib/MainHSK_protocol.h
@@ -0,0 +1,90 @@
+/*
+ * MainHSK_protocol.cpp
+ *
+ * Declares the interface protocol for cross device communication.
+ *
+ * Housekeeping packet consists of header
+ * 0-255 payload bytes
+ * 1 byte CRCS (or checksum)
+ */
+
+/*****************************************************************************
+ * Defines
+ ****************************************************************************/
+#pragma once
+#include <stdint.h>
+
+#define MIN_PERIOD      100
+
+/*******************************************************************************
+ * Typedef enums
+ *******************************************************************************/
+
+/* Command definitions */
+typedef enum MainHSK_cmd {
+  // 2-248 are board-specific: these are test commands
+	eIntSensorRead = 0x02,
+	ePacketCount = 0x03,
+	eMapDevices = 0x04,
+	eHeaterControl = 0x05,
+	eTestHeaterControl = 0x06,
+        eAutoPriorityPeriod = 0x07,
+        eFlows = 0x08,
+        ePressure = 0x09,
+        eCommandPriorities = 0x0A,
+        eISR = 0xA0, //160       
+        eOneWireBridge_conn=0xA9,
+        eOneWireBridge_read=0xAA, // 170
+        eOneWireBridge_report_addresses=0xAB,
+        eSSOut_conn=0xAC,
+        eSSOut=0xAD,
+        eSSOutGround=0xAE, // 174
+        eSSOutHigh=0xAF,
+        eSSOutChannel=0xB0,
+        eSSOutTempTest=0xB1, //177
+        eSetFlowSetPoint=0xB2, //178
+        eScottLyrics = 0xB3, //179
+        eFlow_1_close_valve = 0xB4, // 180
+        eFlow_1_open_valve = 0xB5, //181
+        eFlowDebug=0xB6, //182
+        eFlowRtnGasString=0xB7,  //183
+        eReadCmdPrios=0xB8, //184
+} MainHSK_cmd;
+
+/* Autopriority period */
+typedef struct autoPriorityPeriods {
+  uint16_t lowPriorityPeriod; // auto low pri. period, in ms (0 disables, 100-655)
+  uint16_t medPriorityPeriod; // as above, for med pri.
+  uint16_t highPriorityPeriod;  // as above, for hi pri.
+} autoPriorityPeriods_t;
+
+
+/* SSOut volts */
+typedef struct SSOut_vals {
+  float SFCTemp1;  // was a float, now needs to be a DAC output where each votlage step (5-0)/4096 <-> (125-(-55))/4096.
+  float SFCTemp2;
+  float DCTTemp1;
+  float DCTTemp2;
+  float DCTTemp3;
+  float DCTTemp4;
+  float DCTTemp5;
+  float DCTTemp6;
+  float DCTTemp7;
+  float DCTTemp8;
+  float DCTTemp9;
+  float DCTTemp10;
+  float BatteryTemp1;
+  float BatteryTemp2;
+  uint16_t Solar1;
+  uint16_t Solar2;
+  uint16_t Solar3;
+  uint16_t Solar4;
+} SSOut_vals_t;
+/* not needed anymore
+typedef enum {
+  PRIO_STATE_IDLE = 0, // means it is just the same as eeprom and nothing needs to be done, regardless if locked, unlocked. 
+  PRIO_STATE_UPDATE_DEFAULT = 1, // means series of packets was sent to unlock eeprom and write the new prios (just lock eeprom afterwards).  sends to updated
+  PRIO_STATE_NOT_DEFAULT = 2, // prios have been changed since the eeprom was loaded, doesn't change out until commands are sent. 
+  PRIO_STATE_UPDATED = 3 // write the new version, since it has been changed, occurs after update_default, sends back to idle
+} PrioState;
+*/
